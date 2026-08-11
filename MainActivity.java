e199b1fd94e92cfe6b44653b8a565375654277ba
tounid <tounid147@gmail.com>
2026-08-11 10:40:06 +0600

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,3 @@
+package com.kyamc.studentlookup;
+import android.app.Activity; import android.os.Bundle; import android.webkit.WebView; import android.webkit.WebSettings;
+public class MainActivity extends Activity { public void onCreate(Bundle b){super.onCreate(b); WebView w=new WebView(this); WebSettings s=w.getSettings(); s.setJavaScriptEnabled(true); s.setDomStorageEnabled(true); w.loadUrl("file:///android_asset/index.html"); setContentView(w);} }
